b5cba1b13a8fd11e30321308c30c1432dc0b8b6c
Andrew Hunn <ahunn59@gmail.com>
2024-02-21 14:46:57 -0500

    (chore) add criteria, update homepage, update project specific metadata

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,17 @@
-import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react";
 
 const config: DocsThemeConfig = {
 	logo: <span>Artisanal Futures</span>,
 	project: {
-		link: "https://github.com/shuding/nextra-docs-template",
+		link: "https://github.com/ahunnii/artisanal-futures",
 	},
 	chat: {
 		link: "https://discord.com",
 	},
-	docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+	docsRepositoryBase: "https://github.com/ahunnii/artisanal-futures-documentation",
 	footer: {
-		text: "Nextra Docs Template",
+		text: <p>© {new Date().getFullYear()} Artisanal Futures Docs. All rights reserved. </p>,
 	},
 };
 
